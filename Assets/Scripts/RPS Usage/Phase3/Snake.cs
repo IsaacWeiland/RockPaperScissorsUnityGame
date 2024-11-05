34bfd5d1d461fb435da4f3bd8674ce62d358c912
Isaac Weiland <iweiland7777@gmail.com>
2024-11-05 12:59:53 -0500

    Made all the RPS elements ready

---
+++ Assets/Scripts/RPS Usage/Phase3/Snake.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MouseScriptBase
+{
+    void Start()
+    {
+        controls.OnLeftClick += Controls_OnLeftClick;
+        rend = GetComponent<Renderer>();
+
+    }
+
+    private void Controls_OnLeftClick(object sender, EventArgs e)
+    {
+        
+        if (ReturnMouse && Instance == this)
+        {
+            RunInputEvent(iconListSO, iconSO);
+            rend.material.color = Color.blue;
+        }
+    }
+}
+
